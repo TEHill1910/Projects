7211481b752aef0d5abb5cbcdf4ece4a0630950c
TEHill1910 <thomas_e_hill@outlook.com>
2025-12-03 19:19:48 +0000

    Fix XGBoost model file path

--- F1_tyre_strategy/app.py
+++ F1_tyre_strategy/app.py
@@ -6,7 +6,13 @@ import pandas as pd
 import os
 import glob
 import streamlit as st
+import os
 import joblib
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+model_path = os.path.join(BASE_DIR, "xgb_model.joblib")
+
+model = joblib.load(model_path)
 
 model = joblib.load("xgb_model.joblib")
 
